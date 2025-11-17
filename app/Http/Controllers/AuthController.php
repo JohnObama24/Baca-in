f05e6d02de65a584241dbc141826f9c79f1e69f4
laythanL <laythanlateeshakamiharja@gmail.com>
2025-11-17 19:53:57 +0700

    add Auth & Middleware

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function ShowLogin (){
+      return view('auth.login');
+    }
+
+    public function showRegister (){
+      return view('auth.register');
+    }
+
+    public function Login(Request $requset){
+      $credentials = $requset->validate([
+        "email"=>"required|string|email",
+        "password"=>"required|string",
+      ]);
+
+      if (Auth::attempt($credentials)) {
+        $requset->session()->regenerate();
+       if(Auth::user()->role == "admin"){
+          return redirect()->route('admin-dashboard');
+        }else if(Auth::user()->role == "admin"){
+          return redirect()->route('staf-dashboard');
+       }else{
+        return redirect()->route('user-dashboard');
+      }
+      };
+
+      return back()->withErrors([
+        'email' => 'The provided credentials do not match our records.',
+      ])->onlyInput('email');
+    }
+
+   public function Register (Request $requset){
+      $requset->validate([
+        "name"=>"required|string|max:255",
+        "email"=>"required|string|email|max:255|unique:users",
+        "password"=>"required|string|min:8",
+        "role"=>"required|in:admin,user",
+      ]);
+
+      User::create([
+        "name"=>$requset->name,
+        "email"=>$requset->email,
+        "password"=>bcrypt($requset->password),
+        "role"=>$requset->role,
+      ]);
+
+      return redirect()->route('login')->with('success','Register Success, Please Login');
+    }
+
+    public function Logout (Request $requset){
+      auth::logout();
+      $requset->session()->invalidate();
+      $requset->session()->regenerateToken();
+      return redirect("/");
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('hallo');
 });
 
-Route::get('/halo', function () {
-    return view('hallo');
+Route::get("/login",[AuthController::class,"ShowLogin"])->name("login");
+Route::post("/login",[AuthController::class,"login"]);
+Route::get("/register",[AuthController::class,"showRegister"])->name("register");
+Route::post("/register",[AuthController::class,"Register"]); 
+
+
+Route::middleware('login' )->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
 });

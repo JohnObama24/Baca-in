cc19f3a60690cc3b00dd0f6913c59d9ed12f07cd
JohnObama24 <johnobama24keren@gmail.com>
2025-11-18 08:26:07 +0700

    update user role options, enhance login and register pages styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Bacain - Login</title>
+    <title>Bacain - Register</title>
 </head>
 
 <body class="">
@@ -15,20 +14,36 @@
 
         <!-- hider gambar gatau gambar aoaan -->
         <div class="relative w-full flex-2 bg-tertiary-blue">
-            <img src=""
-                alt="header img" class="w-full h-full object-cover">
+            <img src="" alt="header img" class="w-full h-full object-cover">
         </div>
 
-        <div class="py-6 bg-primary-white flex-[0.5] h-full flex flex-col gap-5 items-center rounded-tl-3xl rounded-tr-3xl">
 
-            <h1 class="text-6xl font-medium text-center">BacaIn</h1>
+        <div
+            class="py-6 bg-primary-white flex-[0.5] h-full flex flex-col gap-5 rounded-tl-3xl rounded-tr-3xl">
+            <p class="text-left text-md ml-4 mt-4">
+                <a href="#" class="text-tertiary-blue font-semibold">← Back To Login</a>
+            </p>
+
+            <h1 class="text-6xl font-medium text-center">Sign Up</h1>
             <p class="text-center text-gray-600 mt-1">
                 Selamat Datang Kembali
             </p>
 
             <form class="mt-6 space-y-4 w-full px-5">
 
-                <div class="bg-white rounded-tl-3xl rounded-br-3xl w-full shadow-md rounded-md px-4 py-3 flex items-center">
+                <div
+                    class="bg-white rounded-tl-3xl rounded-br-3xl w-full shadow-md rounded-md px-4 py-3 flex items-center">
+                    <span class="text-gray-400 mr-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25H4.5A2.25 2.25 0 012.25 17.25V6.75m0 0L12 12.75l9.75-6m-19.5 0A2.25 2.25 0 014.5 4.5h15a2.25 2.25 0 012.25 2.25" />
+                        </svg>
+                    </span>
+                    <input type="text" placeholder="Nama Lengkap" class="w-full bg-transparent focus:outline-none">
+                </div>
+                <div
+                    class="bg-white rounded-tl-3xl rounded-br-3xl w-full shadow-md rounded-md px-4 py-3 flex items-center">
                     <span class="text-gray-400 mr-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" stroke="currentColor"
                             viewBox="0 0 24 24">
@@ -68,15 +83,9 @@
 
                 <button
                     class="w-full bg-tertiary-blue text-white py-3 rounded-tl-3xl rounded-br-3xl rounded-md font-semibold mt-2 shadow-md active:scale-[0.98] transition">
-                    Login
+                    Sign Up
                 </button>
             </form>
-
-            <p class="text-center text-sm mt-4">
-                Don't Have An Account?
-                <a href="#" class="text-blue-500 font-semibold">Sign Up</a>
-            </p>
-
         </div>
     </div>
 
